cf2e4a70221628d785b8c1b26627eeef807389ff
aidostt <leket1999@gmail.com>
2024-02-20 09:09:56 +0600

    Fixed frontend to work with register logic

--- cmd/web/posts.go
+++ cmd/web/posts.go
@@ -9,14 +9,29 @@ import (
 )
 
 type postCreateForm struct {
-	Heading     string   `form:"heading"`
-	Description string   `form:"description"`
-	Tags        []string `form:"tags"`
+	Heading     *string   `form:"heading"`
+	Description *string   `form:"description"`
+	Tags        *[]string `form:"tags"`
 	validator.Validator
 }
 
-//TODO: post should be filtered by time, tags and quantity of likes
+func (app *application) filtersSearchHandlerGet(w http.ResponseWriter, r *http.Request) {
+	d := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "filters.tmpl", d)
+}
+func (app *application) filtersSearchHandlerPost(w http.ResponseWriter, r *http.Request) {
+}
 
+// TODO: post should be filtered by time, tags and quantity of likes
+func (app *application) listPostsHandler(w http.ResponseWriter, r *http.Request) {
+	//d := app.newTemplateData(r)
+	//var input struct {
+	//	Heading string
+	//	Tags    []string
+	//	data.Filters
+	//}
+
+}
 func (app *application) createPostHandlerGet(w http.ResponseWriter, r *http.Request) {
 	d := app.newTemplateData(r)
 	d.Form = postCreateForm{}
@@ -35,13 +50,18 @@ func (app *application) createPostHandlerPost(w http.ResponseWriter, r *http.Req
 	err = authorID.Set("6f52ada3-0c28-45d7-9b35-e420277bc127")
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
+		return
 	}
 	//TODO: retrieve authors id
-	post := &data.Post{
-		AuthorID:    authorID,
-		Heading:     form.Heading,
-		Description: form.Description,
-		Tags:        form.Tags,
+	var post *data.Post
+	if form.Heading != nil {
+		post.Heading = *form.Heading
+	}
+	if form.Description != nil {
+		post.Description = *form.Description
+	}
+	if form.Tags != nil {
+		post.Tags = *form.Tags
 	}
 	form.Validator = *(validator.New())
 	if data.ValidatePost(&(form.Validator), post); !form.Valid() {
@@ -108,18 +128,21 @@ func (app *application) updatePostHandler(w http.ResponseWriter, r *http.Request
 		app.serverErrorResponse(w, r, err)
 		return
 	}
+	if input.Heading != nil {
+		post.Heading = *input.Heading
+	}
+	if input.Description != nil {
+		post.Description = *input.Description
+	}
+	if input.Tags != nil {
+		post.Tags = *input.Tags
+	}
 	var authorID pgtype.UUID
 	err = authorID.Set("6f52ada3-0c28-45d7-9b35-e420277bc127")
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
 	//TODO: retrieve authors id
-	post = &data.Post{
-		AuthorID:    authorID,
-		Heading:     input.Heading,
-		Description: input.Description,
-		Tags:        input.Tags,
-	}
 	input.Validator = *(validator.New())
 	if data.ValidatePost(&(input.Validator), post); !input.Valid() {
 		d.Form = input
